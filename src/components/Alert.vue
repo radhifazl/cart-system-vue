11d314ac9c5b41c37d23245d24fcc95e6d281726
radhifazl <radhifazlinurfahriza@gmail.com>
2023-07-06 11:58:39 +0700

    Menambahkan success alert ketika menambah product ke cart

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <div class="w-fit max-md:w-full right-[50%] translate-x-[50%] rounded-md shadow-xl bg-green-300 p-5 py-4 font-bold absolute z-10"
-        :class="{'translate-y-0': status, '-translate-y-20': !status, 'transition duration-500 ease-in-out': true}"
+        :class="{'translate-y-0': status, '-translate-y-40': !status, 'transition duration-500 ease-in-out': true}"
     >
         <i class='bx bx-fw bx-check-double'></i> Berhasil menambahkan produk ke keranjang!
     </div>
